e7c81f20d31a317fc31c62b4c1d7e6f821f0d803
jingchu.fhb <jingchu.fhb@alibaba-inc.com>
2017-06-02 18:14:30 +0800

    fix: 修复card bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,26 @@
 $('#zoomBtn').click(function() {
   $('.zoom-btn-sm').toggleClass('scale-out');
+  if (!$('.zoom-card').hasClass('scale-out')) {
+    $('.zoom-card').toggleClass('scale-out');
+  }
 });
 
 $('.zoom-btn-sm').click(function() {
-  $('.zoom-card').toggleClass('scale-out');
+  var btn = $(this);
+  var card = $('.zoom-card');
+  // 若未显示则显示, 显示则 go on
+  if ($('.zoom-card').hasClass('scale-out')) {
+    $('.zoom-card').toggleClass('scale-out');
+  }
+  if (btn.hasClass('zoom-btn-person')) {
+    card.css('background-color', '#d32f2f');
+  } else if (btn.hasClass('zoom-btn-doc')) {
+    card.css('background-color', '#ffeb3b');
+  } else if (btn.hasClass('zoom-btn-tangram')) {
+    card.css('background-color', '#388e3c');
+  } else if (btn.hasClass('zoom-btn-report')) {
+    card.css('background-color', '#1976d2');
+  } else {
+    card.css('background-color', '#7b1fa2');
+  }
 });
